20ceddd63f1c84bd20addf72467f77695e9f3014
Tania <taniapcc@gmail>
2026-06-09 19:06:43 -0500

    2 respaldo

--- funciones.js
+++ funciones.js
@@ -1,3 +1,5 @@
+// Archivo funciones.gs
+
 function doGet(e) {
   // Handle GET request parameters
   const params = e.parameter;
@@ -5,7 +7,15 @@ function doGet(e) {
   return HtmlService.createTemplateFromFile('web').evaluate().setTitle("Agenda Google Script");
    
 }
-
+// Cargar archivos html (cabecera,footer,main, etc)
 function obtenerDatosHTML(nombre){
   return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+// obtener datos de la hoja de datos
+function obtenerContactos(){
+  let hoja= SpreadsheetApp.openById('1R--uMJRjDMD1S3B-ul22L9q0ZxUyE5x5LrpdCNxN_-I').getActiveSheet();
+  let datos = hoja.getDataRange().getValues();
+  return datos;
+}
+
